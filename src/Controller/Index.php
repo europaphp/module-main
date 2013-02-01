c80e9cc6dd37a775556e1924f3bb8e9762dd9f0f
Trey Shugart <treshugart@gmail.com>
2013-02-01 12:47:19 +1100

    Initial source code import.

---
+++ src/Controller/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Controller;
+use Europa\Controller\ControllerAbstract;
+
+class Index extends ControllerAbstract
+{
+    public function get()
+    {
+        
+    }
+}
---
+++ views/cli.php
@@ -0,0 +1,9 @@
+
+<?php echo $this->helper('cli')->color('EuropaPHP Command Line Interface', 'cyan'); ?>
+
+<?php echo $this->helper('cli')->color('--------------------------------', 'cyan'); ?>
+
+
+<?php echo trim($this->renderChild()); ?>
+
+
